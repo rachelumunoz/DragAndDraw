f451ea1a096c4e54b0f8752cc9d70154cee848f3
rachelmunoz <rachelumunoz>
2017-10-03 19:46:57 -0700

    Adds custom view

---
+++ app/src/main/java/io/rachelmunoz/draganddraw/BoxDrawingView.java
@@ -0,0 +1,21 @@
+package io.rachelmunoz.draganddraw;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by rachelmunoz on 10/3/17.
+ */
+
+public class BoxDrawingView extends View {
+
+	public BoxDrawingView(Context context) {
+		this(context, null);
+	}
+
+	public BoxDrawingView(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
